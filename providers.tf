467741a2da35cee211b0af679c37b3ccf69ba387
divyaiyer30 <divyaiyer30@gmail.com>
2022-06-25 20:41:32 +0530

    providers

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" { 
+  region     = AWS_DEFAULT_REGION
+  access_key = AWS_ACCESS_KEY_ID
+  secret_key = AWS_DEFAULT_REGION
+}
+
+# allows to access aws account id
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/data-sources/caller_identity
+data "aws_caller_identity" "current" {}
