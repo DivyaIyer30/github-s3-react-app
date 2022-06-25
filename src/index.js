76b3a41f8a08a348957e0ce451a96d6fb9192b6a
divyaiyer30 <divyaiyer30@gmail.com>
2022-06-25 17:02:59 +0530

    link updated

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import axios from 'axios';
 
-const fs = require('fs');
-const loadJSON = JSON.parse(fs.readFileSync('outputs.json'));
-const getUrl = loadJSON.invoke_arn.value;
-const getStage = loadJSON.stage_name.value;
-//const baseURL = getUrl+getStage;
+// const fs = require('fs');
+// const loadJSON = JSON.parse(fs.readFileSync('outputs.json'));
+// const getUrl = loadJSON.invoke_arn.value;
+// const getStage = loadJSON.stage_name.value;
+// const baseURL = getUrl+getStage;
 
 const baseURL =  "https://954agpq9fl.execute-api.us-east-1.amazonaws.com/dev";
 
