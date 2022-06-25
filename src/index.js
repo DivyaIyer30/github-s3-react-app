934961df802a24619294ada7009b664c891f5a60
divyaiyer30 <divyaiyer30@gmail.com>
2022-06-25 16:38:01 +0530

    link updated

--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 import App from './App';
 import axios from "axios";
 
-const fs = require('fs')
-const loadJSON = JSON.parse(fs.readFileSync('./data.json'));
+const fs = require('fs');
+const loadJSON = JSON.parse(fs.readFileSync('outputs.json'));
 const getUrl = loadJSON.invoke_arn.value;
 const getStage = loadJSON.stage_name.value;
 const baseURL = getUrl+getStage;
